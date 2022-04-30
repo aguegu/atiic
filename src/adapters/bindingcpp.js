c0dd1f9b1414edf4d5483db35d80c165d8ac8a87
Weihong Guan <weihong.guan@cpnet.io>
2022-04-30 22:09:47 +0800

    init repo

---
+++ src/adapters/bindingcpp.js
@@ -0,0 +1,55 @@
+import config from 'config';
+import { timeout } from '../utils.js';
+
+class BindingCppAdapter {
+  constructor(spBase, path, logger, timeout = 100) {
+    this.conf = {
+      path,
+      baudRate: 115200,
+      dataBits: 8,
+      parity: 'none',
+      stopBits: 1,
+    };
+    this.logger = logger;
+    // this.spBase = spBase;
+    this.timeout = timeout;
+  }
+
+  async transmit(txString) {
+    const sp = await this.spBase.open(this.conf);
+    const len = txString.length + 521; // 521 = 2: \r\n + 510: 255 bytes in hex + 9: \r\nERROR\r\n
+    const recv = Buffer.alloc(len);
+    let offset = 0;
+    const startAt = Date.now();
+
+    const read = async () => {
+      const { bytesRead } = await sp.read(recv, offset, len - offset);
+      offset += bytesRead;
+      const res = recv.toString().slice(0, offset);
+      this.logger.debug({ rx: res, hex: res.toString('hex'), ttl: Date.now() - startAt });
+
+      if (res.endsWith('OK\r\n')) {
+        const payload = Buffer.from(res.split('\r\n')[1], 'hex');
+        await sp.close();
+        return Promise.resolve(payload);
+      } if (res.endsWith('ERROR\r\n')) {
+        await sp.close();
+        throw new Error('Command Not Supported');
+      } else {
+        return read();
+      }
+    };
+
+    const txBuffer = Buffer.from(`${txString}\r\n`);
+    sp.write(txBuffer);
+    this.logger.debug({ tx: txBuffer.toString(), hex: txBuffer.toString('hex') });
+
+    return timeout(this.timeout, read(), () => {
+      if (sp.isOpen) {
+        sp.close();
+      }
+    });
+  }
+}
+
+export default BindingCppAdapter;
---
+++ src/spl06.js
@@ -0,0 +1,53 @@
+class Spl06 {
+  constructor(adapter) {
+    this.adapter = adapter;
+    this.address = '76';
+    this.coefs = {};
+  }
+
+  async init() {
+    const [id] = await this.adapter.transmit(`AT+TR=${this.address}0D01`);
+    if (id !== 0x10) {
+      throw new Error(`Device at adrress 0x${this.address} IS NOT SPL06`);
+    }
+
+    const [status] = await this.adapter.transmit(`AT+TR=${this.address}0801`); // read cfg
+    if (!(status & 0x40)) {
+      await this.adapter.transmit(`AT+TX=${this.address}0c89`); // soft reset
+      await delay(40);
+    }
+
+    await this.adapter.transmit(`AT+TX=${this.address}0603`); // Pressure 8x sampling
+    await this.adapter.transmit(`AT+TX=${this.address}0783`); // Temperature 8x sampling
+    await this.adapter.transmit(`AT+TX=${this.address}0807`); // Temperature & Pressure
+    await this.adapter.transmit(`AT+TX=${this.address}0900`); // CFG
+
+    const coef = await this.adapter.transmit(`AT+TR=${this.address}1012`);
+    this.coefs.c0 = coef.readInt16BE() >> 4;
+    this.coefs.c1 = (coef.readInt32BE(1) << 4) >> 20;
+    this.coefs.c00 = coef.readInt32BE(3) >> 12;
+    this.coefs.c10 = (coef.readInt32BE(5) << 4) >> 12;
+    this.coefs.c01 = coef.readInt16BE(0x08);
+    this.coefs.c11 = coef.readInt16BE(0x0a);
+    this.coefs.c20 = coef.readInt16BE(0x0c);
+    this.coefs.c21 = coef.readInt16BE(0x0e);
+    this.coefs.c30 = coef.readInt16BE(0x10);
+  }
+
+  async seed() {
+    const payload = await this.adapter.transmit(`AT+TR=${this.address}0006`);
+    const pRaw = payload.readInt32BE() >> 8;
+    const tRaw = (payload.readInt32BE(2) << 8) >>> 8;
+
+    const tSc = tRaw / 7864320;
+    const pSc = pRaw / 7864320;
+
+    const pressure = this.coefs.c00
+      + pSc * (this.coefs.c10 + pSc * (this.coefs.c20 + pSc * this.coefs.c30))
+      + tSc * this.coefs.c01 + tSc * pSc * (this.coefs.c11 + pSc * this.coefs.c21);
+    const celsius = this.coefs.c0 * 0.5 + this.coefs.c1 * tSc;
+    return Promise.resolve({ pressure, celsius });
+  }
+}
+
+export default Spl06;
