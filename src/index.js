9cd3220729050c8c72ebf265bea504631809eba7
aGuegu <weihong.guan@gmail.com>
2022-05-05 21:36:37 +0800

    disable import/export rule in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export * as Aht21 from './devices/aht21.js';
-export * as Spl06 from './devices/spl06.js';
-export * as Pcf8574Hd44780 from './devices/pcf8574_hd44780.js';
+/* eslint-disable import/export */
+export * from './devices/aht21.js';
+export * from './devices/spl06.js';
+export * from './devices/pcf8574_hd44780.js';
