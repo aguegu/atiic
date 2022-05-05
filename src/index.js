d2a376da5f3b8418ae9ad9602263ef8ca3f85997
aGuegu <weihong.guan@gmail.com>
2022-05-05 10:59:33 +0800

    fix path in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export * from 'devices/aht21.js';
-export * from 'devices/spl06.js';
-export * from 'devices/pcf8574_hd44780.js';
+export * from './devices/aht21.js';
+export * from './devices/spl06.js';
+export * from './devices/pcf8574_hd44780.js';
 
-export * from 'adapters/serialport_bindingscpp.js';
+export * from './adapters/serialport_bindingscpp.js';
