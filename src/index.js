d274ce07c7790bcf360b4ec2ded4cef475e9622a
aGuegu <weihong.guan@gmail.com>
2022-05-05 12:50:07 +0800

    no adapter exported in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * as Aht21 from './devices/aht21.js';
 export * as Spl06 from './devices/spl06.js';
 export * as Pcf8574Hd44780 from './devices/pcf8574_hd44780.js';
-export * as SerialportBindingCppAdapter from './adapters/serialport_bindingscpp.js';
+// export * as SerialportBindingCppAdapter from './adapters/serialport_bindingscpp.js';
