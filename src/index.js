ab4f26a42f7b7ca4c4f5d1b9896f78edbe203407
aGuegu <weihong.guan@gmail.com>
2022-05-05 10:57:21 +0800

    fix exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * from 'devices/aht21.js';
-import * from 'devices/spl06.js';
-import * from 'devices/pcf8574_hd44780.js';
+export * from 'devices/aht21.js';
+export * from 'devices/spl06.js';
+export * from 'devices/pcf8574_hd44780.js';
 
-import * from 'adapters/serialport_bindingscpp.js';
+export * from 'adapters/serialport_bindingscpp.js';
