280f7b57a0601dabbc54ad1d6aab16659df848b7
aGuegu <weihong.guan@gmail.com>
2022-05-31 23:28:15 +0800

    tea5767 in vue

--- src/devices/tea5767.js
+++ src/devices/tea5767.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { i2hex } from '../utils.js';
+import { i2hex, delay } from '../utils.js';
 
 class Tea5767 {
   constructor(adapter) {
@@ -29,8 +29,24 @@ class Tea5767 {
     };
   }
 
-  async updateConfigs(configs) {
+  get adjustables() {
+    return _.omit(this.configs, 'ppl', 'searchMode', 'searchUp');
+  }
+
+  async adjust(configs) {
     _.assign(this.configs, configs);
+    return this.setConfigs();
+  }
+
+  async searchNext(isSearchUp) {
+    this.configs.searchMode = true;
+    this.configs.searchUp = isSearchUp;
+    this.configs.ppl += isSearchUp ? 10 : -10;
+    await this.setConfigs();
+    this.configs.searchMode = false;
+
+    await delay(500);
+    return this.getStates();
   }
 
   async setConfigs() {
@@ -56,27 +72,34 @@ class Tea5767 {
     tx[4] |= this.configs.pllref ? 0x80 : 0x00;
     tx[4] |= this.configs.dtc ? 0x40 : 0x00;
 
-    return this.adapter.transmit(`AT+TX=${this.address}${tx.map(i2hex).join()}`);
+    return this.adapter.transmit(`AT+TX=${this.address}${tx.map(i2hex).join('')}`);
+  }
+
+  get frequence() {
+    return (this.configs.ppl * this.xtal) / 4 + this.pplOffset;
   }
 
   get frequenceRange() {
     return this.configs.bl ? [76000000, 91000000] : [87500000, 108000000];
   }
 
-  setFrequence(hz) {
-    const [min, max] = this.frequenceRange();
+  async setFrequence(hz) {
+    const [min, max] = this.frequenceRange;
     if (hz < min || hz > max) {
       throw new Error('frequence out of range');
     }
     this.configs.ppl = parseInt(((hz - this.pplOffset) * 4) / this.xtal, 10);
+    return this.setConfigs();
   }
 
   async getStates() {
-    const regs = await this.adapter.transmit(`AT+RX=${this.address}05`).then(Array.from);
+    const regs = await this.adapter.transmit(`AT+TR=${this.address}05`).then(Array.from);
+    const ppl = ((regs[0] & 0x3f) << 8) + regs[1];
+    this.configs.ppl = ppl;
     return {
       isReady: !!(regs[0] & 0x80),
       isBandLimitReached: !!(regs[0] & 0x40),
-      ppl: ((regs[0] & 0x3f) << 8) + regs[1],
+      ppl,
       isStereo: !!(regs[2] & 0x80),
       ifCounter: regs[2] & 0x7f,
       level: regs[3] >> 4,
