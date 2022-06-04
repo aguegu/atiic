26fa1fb614ca301aeadb0a2b8e9ee26fc78ea2cf
aGuegu <weihong.guan@gmail.com>
2022-06-04 23:16:05 +0800

    accelerometer/gyroscope range

--- src/devices/mpu6050.js
+++ src/devices/mpu6050.js
@@ -1,32 +1,47 @@
 import { i2hex } from '../utils.js';
 
+const accelerometerScales = ['2', '4', '8', '16'];
+const gyroscopeScales = ['250', '500', '1000', '2000'];
+
 class Mpu6050 {
-  constructor(adapter) {
+  constructor(adapter, address = '68') {
     this.adapter = adapter;
-    this.address = '68';
+    this.address = address;
   }
 
-  async init() {
+  async init(gyroscopeScale = '500', accelerometerScale = '4') {
+    const gfs = gyroscopeScales.indexOf(gyroscopeScale);
+    if (gfs < 0) {
+      throw new Error('invalid gyroscopeScale');
+    }
+
+    const afs = accelerometerScales.indexOf(accelerometerScale);
+    if (afs < 0) {
+      throw new Error('invalid accelerometerScale');
+    }
+
+    this.gyroscopeSensitivity = (131072 >> gfs) / 1000;
+    this.accelerometerSensitivity = 16384 >> afs;
     await this.adapter.transmit(`AT+TX=${this.address}${[0x6b, 0x00].map(i2hex).join('')}`);
-    await this.adapter.transmit(`AT+TX=${this.address}${[0x19, 0x07].map(i2hex).join('')}`);
+    await this.adapter.transmit(`AT+TX=${this.address}${[0x19, 0x0f].map(i2hex).join('')}`);
     await this.adapter.transmit(`AT+TX=${this.address}${[0x1a, 0x04].map(i2hex).join('')}`);
-    await this.adapter.transmit(`AT+TX=${this.address}${[0x1b, 0x08].map(i2hex).join('')}`);
-    await this.adapter.transmit(`AT+TX=${this.address}${[0x1c, 0x08].map(i2hex).join('')}`);
+    await this.adapter.transmit(`AT+TX=${this.address}${[0x1b, gfs << 3].map(i2hex).join('')}`);
+    await this.adapter.transmit(`AT+TX=${this.address}${[0x1c, afs << 3].map(i2hex).join('')}`);
   }
 
   async measure() {
     const payload = await this.adapter.transmit(`AT+TR=${this.address}3B0E`);
     return {
       accelerometers: {
-        x: payload.readInt16BE(),
-        y: payload.readInt16BE(2),
-        z: payload.readInt16BE(4),
+        x: payload.readInt16BE() / this.accelerometerSensitivity,
+        y: payload.readInt16BE(2) / this.accelerometerSensitivity,
+        z: payload.readInt16BE(4) / this.accelerometerSensitivity,
       },
-      temperature: payload.readInt16BE(6),
+      temperature: payload.readInt16BE(6) / 340 + 36.53,
       gyroscopes: {
-        x: payload.readInt16BE(8),
-        y: payload.readInt16BE(10),
-        z: payload.readInt16BE(12),
+        x: payload.readInt16BE(8) / this.gyroscopeSensitivity,
+        y: payload.readInt16BE(10) / this.gyroscopeSensitivity,
+        z: payload.readInt16BE(12) / this.gyroscopeSensitivity,
       },
     };
   }
